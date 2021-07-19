bc7913861534c177a892fa0655313e2923dbcb7f
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2021-07-19 23:53:23 +0300

    add banner section

--- CineMagic/Web/CineMagic.Web.ViewModels/Movies/MovieHomePageBannerViewModel.cs
+++ CineMagic/Web/CineMagic.Web.ViewModels/Movies/MovieHomePageBannerViewModel.cs
@@ -2,19 +2,28 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
+    using AutoMapper;
     using CineMagic.Data.Models;
     using CineMagic.Services.Mapping;
 
-    public class MovieHomePageBannerViewModel : IMapFrom<Movie>
+    public class MovieHomePageBannerViewModel : IMapFrom<Movie>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
         public string Title { get; set; }
 
-        public ICollection<MovieBackdropsViewModel> Backdrops { get; set; }
+        public string Backdrops { get; set; }
 
         public string TrailerPath { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Movie, MovieHomePageBannerViewModel>()
+                .ForMember(m => m.Backdrops, opt => opt.MapFrom(x => string.Join(", ", x.Backdrops.Select(x => "\"" + x.Path + "\""))));
+                
+        }
     }
 }
