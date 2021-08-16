47842d59235c3e0f1b7bca82af3f9d4c892824c7
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2021-08-16 12:58:16 +0300

    add in memory cachin for home controller

--- CineMagic/Web/CineMagic.Web/Controllers/HomeController.cs
+++ CineMagic/Web/CineMagic.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿namespace CineMagic.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Linq;
     using System.Threading.Tasks;
@@ -10,6 +12,7 @@
     using CineMagic.Web.ViewModels.Movies;
     using CineMagic.Web.ViewModels.Privacies;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Memory;
 
     public class HomeController : Controller
     {
@@ -20,27 +23,104 @@
 
         private readonly IMoviesService moviesService;
         private readonly IPrivaciesService privaciesService;
+        private readonly IMemoryCache cache;
 
         public HomeController(
             IMoviesService moviesService,
-            IPrivaciesService privaciesService)
+            IPrivaciesService privaciesService,
+            IMemoryCache cache)
         {
             this.moviesService = moviesService;
             this.privaciesService = privaciesService;
+            this.cache = cache;
         }
 
         public async Task<IActionResult> Index()
         {
+            const string mostRecentMovieCacheKey = "MostRecentMovieCacheKey";
+            const string recentMoviesCacheKey = "RecentMoviesCacheKey";
+            const string mostPopularMovieCacheKey = "MostPopularMovieCacheKey";
+            const string popularMoviesCacheKey = "PopularMoviesCacheKey";
+            const string bestRatedMovieCacheKey = "BestRatedMovieCacheKey";
+            const string topRatedMoviesCacheKey = "TopRatedMoviesCacheKey";
+            const string latestMoviesCacheKey = "LatestMoviesCacheKey";
+
+            var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromDays(1));
+
+            var mostRecentMovie = this.cache.Get<IEnumerable<MovieRespTabsViewModel>>(mostRecentMovieCacheKey);
+
+            if (mostRecentMovie == null)
+            {
+                mostRecentMovie = await this.moviesService
+                    .GetRecentMoviesAsync<MovieRespTabsViewModel>(HomePageLeadMovieCount);
+
+                this.cache.Set(mostRecentMovieCacheKey, mostRecentMovie, cacheOptions);
+            }
+
+            var recentMovies = this.cache.Get<IEnumerable<MovieStandartViewModel>>(recentMoviesCacheKey);
+
+            if (recentMovies == null)
+            {
+                recentMovies = await this.moviesService
+                    .GetRecentMoviesAsync<MovieStandartViewModel>(HomePageTabFeaturedMoviesCount);
+
+                this.cache.Set(recentMoviesCacheKey, recentMovies, cacheOptions);
+            }
+
+            var mostPopularMovie = this.cache.Get<IEnumerable<MovieRespTabsViewModel>>(mostPopularMovieCacheKey);
+
+            if (mostPopularMovie == null)
+            {
+                mostPopularMovie = await this.moviesService
+                    .GetPopularMoviesAsync<MovieRespTabsViewModel>(HomePageLeadMovieCount);
+
+                this.cache.Set(mostPopularMovieCacheKey, mostPopularMovie, cacheOptions);
+            }
+
+            var popularMovies = this.cache.Get<IEnumerable<MovieStandartViewModel>>(popularMoviesCacheKey);
+
+            if (popularMovies == null)
+            {
+                popularMovies = await this.moviesService
+                    .GetPopularMoviesAsync<MovieStandartViewModel>(HomePageTabFeaturedMoviesCount);
+
+                this.cache.Set(popularMoviesCacheKey, popularMovies, cacheOptions);
+            }
+
+            var bestRatedMovie = this.cache.Get<IEnumerable<MovieRespTabsViewModel>>(bestRatedMovieCacheKey);
+
+            if (bestRatedMovie == null)
+            {
+                bestRatedMovie = await this.moviesService
+                    .GetTopRatedMoviesAsync<MovieRespTabsViewModel>(HomePageLeadMovieCount);
+
+                this.cache.Set(bestRatedMovieCacheKey, bestRatedMovie, cacheOptions);
+            }
+
+            var topRatedMovies = this.cache.Get<IEnumerable<MovieStandartViewModel>>(topRatedMoviesCacheKey);
+
+            if (topRatedMovies == null)
+            {
+                topRatedMovies = await this.moviesService
+                    .GetTopRatedMoviesAsync<MovieStandartViewModel>(HomePageTabFeaturedMoviesCount);
+
+                this.cache.Set(topRatedMoviesCacheKey, topRatedMovies, cacheOptions);
+            }
+
+            var latestMovies = this.cache.Get<IEnumerable<MovieStandartViewModel>>(latestMoviesCacheKey);
+
+            if (latestMovies == null)
+            {
+                latestMovies = await this.moviesService
+                    .GetLatestMoviesAsync<MovieStandartViewModel>(HomePageLatestMoviesCount);
+
+                this.cache.Set(latestMoviesCacheKey, latestMovies, cacheOptions);
+            }
+
             var userId = this.User.GetId();
 
             var bannerSectionMovie = await this.moviesService.GetBannerSectionMovieAsync<MovieHomePageBannerViewModel>();
-            var mostRecentMovie = await this.moviesService.GetRecentMoviesAsync<MovieRespTabsViewModel>(HomePageLeadMovieCount);
-            var recentMovies = await this.moviesService.GetRecentMoviesAsync<MovieStandartViewModel>(HomePageTabFeaturedMoviesCount);
-            var mostPopularMovie = await this.moviesService.GetPopularMoviesAsync<MovieRespTabsViewModel>(HomePageLeadMovieCount);
-            var popularMovies = await this.moviesService.GetPopularMoviesAsync<MovieStandartViewModel>(HomePageTabFeaturedMoviesCount);
-            var bestRatedMovie = await this.moviesService.GetTopRatedMoviesAsync<MovieRespTabsViewModel>(HomePageLeadMovieCount);
-            var topRatedMovies = await this.moviesService.GetTopRatedMoviesAsync<MovieStandartViewModel>(HomePageTabFeaturedMoviesCount);
-            var latestMovies = await this.moviesService.GetLatestMoviesAsync<MovieStandartViewModel>(HomePageLatestMoviesCount);
             var watchlistMovies = await this.moviesService.GetWatchlistMoviesAsync<MovieWatchlistViewModel>(userId, HomePageWatchlistMoviesCount);
 
             var viewModels = new HomePageViewModelsList
