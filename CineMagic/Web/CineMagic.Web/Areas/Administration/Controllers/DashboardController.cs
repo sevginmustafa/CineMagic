3ef1c771fdd093fcdec1a346215681fad27a526d
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2022-01-17 11:47:56 +0200

    add amin area button

--- CineMagic/Web/CineMagic.Web/Areas/Administration/Controllers/DashboardController.cs
+++ CineMagic/Web/CineMagic.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,18 +1,9 @@
 ﻿namespace CineMagic.Web.Areas.Administration.Controllers
 {
-    using CineMagic.Services.Data.Contracts;
-
     using Microsoft.AspNetCore.Mvc;
 
     public class DashboardController : AdministrationController
     {
-        private readonly ISettingsService settingsService;
-
-        public DashboardController(ISettingsService settingsService)
-        {
-            this.settingsService = settingsService;
-        }
-
         [HttpGet("/Administration")]
         public IActionResult Index()
         {
